b50beed37f3fef94433675f4d6d508b7c60ae3bb
mandappp <113966919+mandappp@users.noreply.github.com>
2022-09-20 13:06:03 +0700

    Add files via upload

---
+++ Praktikum 1/Modul 1 - Latihan 1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@Materi: Variabel dan Pemrograman Dasar
+@Judul: Praktikum 1 Latihan 1
+@Hari/Tanggal: Senin, 20210920
+@NIM: 064002000010
+@author: Amanda Putridana
+"""
+
+nama = input("Nama kamu siapa? ")
+asal = input("Darimana asal kamu? " + nama + "? ")
+sekolah = input("Sebelumnya bersekolah dimana? ")
+
+print("\nHallo selamat siang " + nama)
+print("Wah dari " + asal)
+print("Pasti menyenangkan bersekolah di " + sekolah)
